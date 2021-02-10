523c5d6df4472415eb0ce5aea94b923f76cd42ae
AndreyAD <arakelyants.andrey@yandex.ru>
2021-02-11 00:46:02 +0300

    Prepare to initialize a database

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+
+load_dotenv(find_dotenv())
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
