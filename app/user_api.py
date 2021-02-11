d3d9d916d2de7759802e9443a469ad8d5e9dc256
AndreyAD <arakelyants.andrey@yandex.ru>
2021-02-12 01:26:17 +0300

    Use raw SQL only

--- app/user_api.py
+++ app/user_api.py
@@ -10,18 +10,31 @@ def create_user():
     data = request.get_json() or {}
     if 'username' not in data or 'email' not in data or 'password' not in data:
         return bad_request('must include username, email and password fields')
-    if Users.query.filter_by(username=data['username']).first():
+    name_query = f"""
+    SELECT users.id
+    FROM users WHERE users.username = '{data['username']}' LIMIT 1
+    """
+    query_result_proxy = database.session.execute(name_query)
+    query_result = [r for r in query_result_proxy]
+    if query_result:
         return bad_request('please use a different username')
-    if Users.query.filter_by(email=data['email']).first():
+
+    email_query = f"""
+    SELECT users.id
+    FROM users WHERE users.email = '{data['email']}' LIMIT 1
+    """
+    query_result_proxy = database.session.execute(email_query)
+    query_result = [r for r in query_result_proxy]
+    if query_result:
         return bad_request('please use a different email address')
+
     user = Users()
     user.from_dict(data, new_user=True)
-    insert_template = "INSERT INTO users (username, email, password_hash) VALUES ('{}', '{}', '{}') RETURNING users.id"
-    insert_command = insert_template.format(
-        user.username,
-        user.email,
-        user.password_hash
-    )
+    insert_command = f"""
+    INSERT INTO users (username, email, password_hash) 
+    VALUES ('{user.username}', '{user.email}', '{user.password_hash}') 
+    RETURNING users.id
+    """
     database.session.execute(insert_command)
     database.session.commit()
     response = jsonify({})
