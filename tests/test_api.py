696d398ba6b769bfe33959456f2500aa2618660c
AndreyAD <arakelyants.andrey@yandex.ru>
2021-02-14 15:44:27 +0300

    Write the token test

--- tests/test_api.py
+++ tests/test_api.py
@@ -26,3 +26,29 @@ def test_create_user():
     user_id = response_json.get('user_id')
     assert user_id
     assert isinstance(user_id, int)
+
+
+def test_get_token():
+    fake = Faker()
+    user_info = {
+        'username': fake.name(),
+        'email': fake.email(),
+        'password': 'pass'
+    }
+    logger.info(f'Create the user: {user_info}')
+    response = requests.post(
+        'http://127.0.0.1:5000/api/v1/users/create',
+        json=user_info
+    )
+    logger.info(f'Get response: {response.text}')
+
+    response = requests.post(
+        f'http://127.0.0.1:5000/api/v1/tokens',
+        auth=(user_info['username'], user_info['password'])
+    )
+    logger.info(f'Get response: {response.text}')
+    assert response.status_code == 200
+    response_json = response.json()
+    assert len(response_json) == 1
+    token = response_json.get('token')
+    assert token
