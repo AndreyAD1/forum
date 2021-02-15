d15f424d255345eba32c5e4f1979d8d4cd4b920b
AndreyAD <arakelyants.andrey@yandex.ru>
2021-02-15 08:21:49 +0300

    Improve the readme file

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,10 @@ app = Flask(__name__)
 app.config.from_object(Config)
 if app.config['VERBOSE']:
     app.logger.setLevel(logging.DEBUG)
+    database = SQLAlchemy(app, engine_options={'echo': 'debug'})
+else:
+    database = SQLAlchemy(app)
 
-database = SQLAlchemy(app, engine_options={'echo': 'debug'})
 migrate = Migrate(app, database)
 
 from app import models, user_api, api_auth, api_tokens, post_api
