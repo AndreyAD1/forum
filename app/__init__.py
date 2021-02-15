28478e9eaecc2cd1dd33990f7f151f80d27ab139
AndreyAD <arakelyants.andrey@yandex.ru>
2021-02-15 06:52:47 +0300

    Delete the redundant route

--- app/__init__.py
+++ app/__init__.py
@@ -13,5 +13,5 @@ if app.config['VERBOSE']:
 database = SQLAlchemy(app, engine_options={'echo': 'debug'})
 migrate = Migrate(app, database)
 
-from app import routes, models, user_api, api_auth, api_tokens, post_api
+from app import models, user_api, api_auth, api_tokens, post_api
 from app import thread_api, forum_api
